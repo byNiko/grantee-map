41e4b373011ae10c995662957cf9c934060af0fd
nick <nick@byniko.com>
2026-07-16 16:38:05 -0700

    remove shadow

--- grantee-map.php
+++ grantee-map.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: Grantee Map
  * Description: Interactive Leaflet map of grantee organizations with client-side filtering by organization type.
- * Version:     2.2.0
+ * Version:     2.2.3
  * Author:      ByNiko and 3N Design
  * Text Domain: grantee-map
  * Plugin URI: https://github.com/byniko/grantee-map
